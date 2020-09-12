9e35e83fcfc3d43ed4b6405afbd2b933bbc1240d
will-malpass <71153169+will-malpass@users.noreply.github.com>
2020-09-12 12:30:19 +0100

    Create schema_creation.sql

---
+++ mysql_scripts/schema_creation.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA `rdd_rob` ;  /*for hosting tables for generating robustness test data */
+CREATE SCHEMA `tallies` ;  /*for hosting tables for counts (account worth, new/existing/total friends etc...*/
+CREATE SCHEMA `networks` ; /*for hosting tables for generating social networks*/
+CREATE SCHEMA `in_means` ; /*for hosting tables for generating the average group outcome*/
+CREATE SCHEMA `exports` ;  /*for hosting tables ready for csv export*/
+CREATE SCHEMA `sdb` ;      /*for hosting the steam dataset, leave this untouched*/
+
